a75f2b7e5aae96269308767266edf4ce1a105172
ashnik-nova <ashnik2511@gmail.com>
2024-10-04 15:31:47 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   public/blob.svg
    	new file:   public/iot.jpg
    	deleted:    src/assets/iot.jpeg
    	modified:   src/components/Home/Home.jsx
    
    Changes not staged for commit:
    	modified:   index.html
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/components/About/About.jsx
    	deleted:    src/components/Connect/Connect.jsx
    	modified:   src/components/Header/Header.jsx
    	modified:   src/components/Home/Home.jsx
    	deleted:    src/components/Transmit/Transmit.jsx
    	modified:   src/main.jsx
    
    Untracked files:
    	src/components/Dashboard/
    	src/components/InnerHome/

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,60 @@
-import React from 'react';
-import iot from 'B:/Coding Stuffs/React/cs-project/secure-iot/src/assets/iot.jpeg'; // Assuming the image is in the same directory as your component
+import React, { useState } from "react";
 
 function Home() {
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
-    <div className="bg-slate-300 p-2 h-96 flex pt-8">
-        <img src={iot} alt=""
-        className='rounded-xl mt-5 w-1/3 h-4/5'
-        />
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. In repellat, neque at accusamus dolorum, modi eius id libero possimus iure sint doloremque cumque ipsum ducimus totam. Dignissimos laudantium obcaecati numquam.
-          </p>
-          <button>
-            Get Started
-            
-          </button>
+    <>
+      <div className="h-screen bg-cover bg-center bg-[url('/iot.jpg')]">
+        <div className="flex justify-between bg-black bg-opacity-50 h-full p-10">
+          {/* Left Section */}
+          <div className="flex flex-col items-start justify-center w-1/2 pl-10">
+            <div className="bg-black bg-opacity-25 p-10 rounded-lg shadow-inner transition-all hover:shadow-lg duration-700">
+              <h1 className="text-white text-5xl font-bold text-center">
+                Welcome to Secure-IoT
+              </h1>
+              <p className="text-white text-lg mb-8">
+                Monitor and manage your IoT devices effortlessly.
+              </p>
+              <a
+                href="/features"
+                className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600"
+              >
+                Explore Features
+              </a>
+            </div>
+          </div>
+          {/* Right Section with SVG blob and content */}
+          <div className="relative w-1/2 flex items-center justify-center">
+            {/* SVG blob as background with hover effect */}
+            <div 
+              className={`absolute inset-0 bg-cover bg-center bg-[url('/blob.svg')] transition-transform duration-500 ${
+                isHovering ? 'scale-105' : 'scale-100'
+              }`}
+              onMouseEnter={() => setIsHovering(true)}
+              onMouseLeave={() => setIsHovering(false)}
+            ></div>
+            {/* Content inside the blob */}
+            <div className="relative z-10 text-center max-w-[75%] mx-auto">
+              <h2 className="text-white text-4xl font-bold mb-4">
+                Get Started
+              </h2>
+              <p className="text-white text-lg mb-8">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                veniam inventore debitis iusto omnis laudantium delectus vitae
+                magni fugiat aut. Nam, unde placeat!
+              </p>
+              <a
+                href="/get-started"
+                className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600"
+              >
+                Start Now
+              </a>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
+    </>
   );
 }
 
